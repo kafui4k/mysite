ab18fe772f44c8217c8be2819171a8bcee84a668
Kafui Alordo <kalordo7@gmail.com>
2019-05-02 03:32:31 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="kafui_logo.jfif" className="App-logo" alt="me" />
+      <header>
+        <h3>Email: kalordo7@gmail.com | <a href="https://www.github.com/Kafui4k">GitHub: Kafui4k</a></h3>
+        <h1><small>Hello</small> Kafui ALORDO</h1>
+         <p>Freelance Creative | Software Developer</p>
+         <p>Freelance Creative | Software Developer</p>
+        </header>
+    </div>
+  );
+}
+
+export default App;
