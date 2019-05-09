c801ea8372a1511914559d9890b504ca3074705d
Kafui Alordo <kalordo7@gmail.com>
2019-05-09 13:56:26 +0000

    update profile

--- src/App.js
+++ src/App.js
@@ -6,10 +6,49 @@ function App() {
     <div className="App">
       <img src="kafui_logo.jfif" className="App-logo" alt="me" />
       <header>
-        <h3>Email: kalordo7@gmail.com | <a href="https://www.github.com/Kafui4k">GitHub: Kafui4k</a></h3>
-        <h1><small>Hello</small> Kafui ALORDO</h1>
-         <p>Freelance Creative | Software Developer</p>
-         <p>Freelance Creative | Software Developer</p>
+        <h1>
+          <small>Hello am,</small>
+          <br/> Kafui ALORDO
+        </h1>
+         <p>
+           Am a Creative | Software Developer Building Web and mobile app 
+           solutions with tools like Python/Django | 
+           JavaScript - React/React Native.
+          </p>
+          <p>
+            With 4 years of experience in Web | Mobile Applications 
+            developement.
+          </p>
+          <p>
+            Am currently freelancing @webuildandsupport, 
+            a Team lead/organizer @PythoHo and a member of the 
+            Python/Django Community in Ghana.
+          </p>
+
+          <h1>For HIRE:</h1>
+          <p>Am up for Remote | in-person work.</p>
+
+          <h3>Lets get Social:</h3>
+          <p>
+            Email: kalordo7@gmail.com
+          </p>
+          <p>
+            <a href="https://www.github.com/kafui4k" target="blank">
+              GitHub: kafui4k
+            </a>
+          </p>
+          <p>
+            <a href="https://www.twitter.com/kafui4k">
+              Twitter: @kafui4k
+            </a>
+          </p>
+          <p>
+            Instagram: @kafui4k
+          </p>
+          <p>
+            LinkedIn: @kafui4k
+          </p>
+
         </header>
     </div>
   );
